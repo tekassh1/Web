4a00753006d65cf7face8c20ba11777e7ebda3e1
geeks <geekslovee@gmail.com>
2024-01-01 20:26:32 +0300

    dirs fixed.

--- Lab_4/Backend/frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { MainHeader } from "./app/components/header/header.component";
-import { MainFooter } from "./app/components/footer/footer.component";
-
-bootstrapApplication(MainHeader).catch(e => console.error(e));
-bootstrapApplication(MainFooter).catch(e => console.error(e));
