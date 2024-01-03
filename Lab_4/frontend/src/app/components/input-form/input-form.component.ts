a2deaa8d2b4fd4bcc86cae3086e718dd667c730b
geeks <geekslovee@gmail.com>
2024-01-03 15:56:02 +0300

    Form validation completed.

--- Lab_4/frontend/src/app/components/input-form/input-form.component.ts
+++ Lab_4/frontend/src/app/components/input-form/input-form.component.ts
@@ -1,10 +1,15 @@
 import {Component} from "@angular/core";
 import {FormsModule, FormGroup, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+
+interface vObj {
+    [s: string]: boolean;
+}
 
 @Component({
     selector: "main-form",
     standalone: true,
-    imports: [FormsModule, ReactiveFormsModule],
+    imports: [FormsModule, ReactiveFormsModule, NgIf],
     styleUrls: ["input-form.css"],
     template: `
         <form [formGroup]="coordsForm" novalidate (ngSubmit)="submit()">
@@ -12,25 +17,42 @@ import {FormsModule, FormGroup, FormControl, ReactiveFormsModule, Validators} fr
                 Enter<a class="colorfulText"> X </a>value<br/>
 
                 <input name="xCoord" formControlName="xCoord"/>
-                @if (coordsForm.controls['xCoord'].invalid && coordsForm.controls['xCoord'].touched) {
-                    <div>Wrong X value!</div>
-                }
+
+                <div *ngIf="coordsForm.controls['xCoord'].hasError('rangeErr') && coordsForm.controls['xCoord'].touched"
+                     class="wrongInputMsg">
+                    Wrong 'x' value! Available range ({{xMin}} ; {{xMax}})
+                </div>
+                <div *ngIf="coordsForm.controls['xCoord'].hasError('NaN') && coordsForm.controls['xCoord'].touched"
+                     class="wrongInputMsg">
+                    Wrong 'x' value! Value should be a numeric!
+                </div>
             </div>
             <div class="selectionBlock">
                 Enter<a class="colorfulText"> Y </a>value<br/>
 
                 <input name="yCoord" formControlName="yCoord"/>
-                <!--                @if (coordsForm.controls["yCoord"].invalid && coordsForm.controls["yCoord"].touched) {-->
-                <!--                    <div class="alert">Wrong Y value!</div>-->
-                <!--                }-->
+
+                <div *ngIf="coordsForm.controls['yCoord'].hasError('rangeErr') && coordsForm.controls['yCoord'].touched"
+                     class="wrongInputMsg">
+                    Wrong 'y' value! Available range ({{yMin}} ; {{yMax}})
+                </div>
+                <div *ngIf="coordsForm.controls['yCoord'].hasError('NaN') && coordsForm.controls['yCoord'].touched"
+                     class="wrongInputMsg">
+                    Wrong 'y' value! Value should be a numeric!
+                </div>
             </div>
             <div class="selectionBlock">
                 Enter<a class="colorfulText"> R </a>value<br/>
 
                 <input name="rValue" formControlName="rValue"/>
-                <!--                @if (coordsForm.controls["rValue"].invalid && coordsForm.controls["rValue"].touched) {-->
-                <!--                    <div class="alert">Wrong R value!</div>-->
-                <!--                }-->
+                <div *ngIf="coordsForm.controls['rValue'].hasError('rangeErr') && coordsForm.controls['rValue'].touched"
+                     class="wrongInputMsg">
+                    Wrong 'r' value! Available range ({{rMin}} ; {{rMax}})
+                </div>
+                <div *ngIf="coordsForm.controls['rValue'].hasError('NaN') && coordsForm.controls['rValue'].touched"
+                     class="wrongInputMsg">
+                    Wrong 'r' value! Value should be a numeric!
+                </div>
             </div>
             <div id="submitBlock">
                 <button type="submit" id="checkRequestBtn">Check</button>
@@ -45,20 +67,61 @@ export class MainForm {
 
     constructor() {
         this.coordsForm = new FormGroup({
-            "xCoord": new FormControl("", [Validators.required, this.xCoordValidator]),
-            "yCoord": new FormControl("", Validators.required),
-            "rValue": new FormControl("", Validators.required)
+            "xCoord": new FormControl("", [Validators.required, this.xCoordValidator.bind(this)]),
+            "yCoord": new FormControl("", [Validators.required, this.yCoordValidator.bind(this)]),
+            "rValue": new FormControl("", [Validators.required, this.rValueValidator.bind(this)])
         });
     }
 
     submit() {
-        console.log(this.coordsForm);
+        if (!this.coordsForm.invalid) {
+            console.log("x: " + this.coordsForm.get('xCoord').value);
+            console.log("y: " + this.coordsForm.get('yCoord').value);
+            console.log("r: " + this.coordsForm.get('rValue').value);
+        }
     }
 
-    xCoordValidator(control: FormControl): { [s: string]: boolean } | null {
-        if (control.value < -3 || control.value > 5) {
-            return {"xCoord": true};
+    xMin: number = -3;
+    xMax: number = 5;
+    yMin: number = -5;
+    yMax: number = 5;
+    rMin: number = -3;
+    rMax: number = 5;
+
+
+    xCoordValidator(control: FormControl): {[s: string]: boolean} | null {
+        let validationMap: vObj = {};
+
+        if ((isNaN(control.value))) {
+            validationMap['NaN'] = true;
         }
-        return null;
+        else if ((control.value < this.xMin) || (control.value > this.xMax)) {
+            validationMap['rangeErr'] = true;
+        }
+        return validationMap;
+    }
+
+    yCoordValidator(control: FormControl): {[s: string]: boolean} | null {
+        let validationMap: vObj = {};
+
+        if ((isNaN(control.value))) {
+            validationMap['NaN'] = true;
+        }
+        else if ((control.value < this.yMin) || (control.value > this.yMax)) {
+            validationMap['rangeErr'] = true;
+        }
+        return validationMap;
+    }
+
+    rValueValidator(control: FormControl): {[s: string]: boolean} | null {
+        let validationMap: vObj = {};
+
+        if ((isNaN(control.value))) {
+            validationMap['NaN'] = true;
+        }
+        else if ((control.value < this.rMin) || (control.value > this.rMax)) {
+            validationMap['rangeErr'] = true;
+        }
+        return validationMap;
     }
 }
